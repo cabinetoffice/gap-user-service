8d5a4ca601474a30bb0a5f5772f9a608eb2bad17
Gavin Cook <gavin.cook@and.digital>
2023-02-09 16:24:12 +0000

    adding spring boot project

---
+++ src/test/java/gov/cabinetofice/gapuserservice/GapUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.cabinetofice.gapuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GapUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
