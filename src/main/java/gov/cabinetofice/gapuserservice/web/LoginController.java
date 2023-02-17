28c19b65343931766361c1788f9bba2da4b20d88
Gavin Cook <gavin.cook@and.digital>
2023-02-17 15:33:34 +0000

    renaming a variable and adding a constant

--- src/main/java/gov/cabinetofice/gapuserservice/web/LoginController.java
+++ src/main/java/gov/cabinetofice/gapuserservice/web/LoginController.java
@@ -16,25 +16,26 @@ import org.springframework.web.servlet.view.RedirectView;
 public class LoginController {
 
     private final ThirdPartyAuthProviderProperties authenticationProvider;
-    private final ThirdPartyJwtService thirdPartyJwtService;
+    private final ThirdPartyJwtService colaJwtService;
+    private static final String REDIRECT_URL_COOKIE = "redirectUrl";
 
     @GetMapping("/login")
     public RedirectView login(final @CookieValue(name = "find-grants-test", required = false) String jwt,
                               final @RequestParam String redirectUrl,
                               final HttpServletResponse response) {
-        response.addCookie(new Cookie("redirectUrl", redirectUrl));
+        response.addCookie(new Cookie(REDIRECT_URL_COOKIE, redirectUrl));
 
         if (jwt == null) return redirectToThirdParty();
 
         // TODO currently validating against the third party. When we switch to using custom JWTs, we'll want to validate against our own definition instead
-        final boolean isJwtValid = thirdPartyJwtService.isTokenValid(jwt);
+        final boolean isJwtValid = colaJwtService.isTokenValid(jwt);
         if (!isJwtValid) return redirectToThirdParty();
 
         return new RedirectView("/redirect-after-login");
     }
 
     @GetMapping("/redirect-after-login")
-    public RedirectView redirectOnLogin(@CookieValue(name = "redirectUrl") String redirectUrl) {
+    public RedirectView redirectOnLogin(@CookieValue(REDIRECT_URL_COOKIE) String redirectUrl) {
         // TODO we'll want to validate the third party JWT here once we use custom JWTs
         return new RedirectView(redirectUrl);
     }
