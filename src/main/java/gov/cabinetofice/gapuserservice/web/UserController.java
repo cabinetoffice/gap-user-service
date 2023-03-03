f81aa8b03273e186d5b8372b7da8fc32915041ad
Gavin Cook <gavin.cook@and.digital>
2023-03-03 11:58:09 +0000

    Adding files to save users to cognito

--- src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
+++ src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
@@ -1,18 +1,23 @@
 package gov.cabinetofice.gapuserservice.web;
 
 import gov.cabinetofice.gapuserservice.dto.CreateUserDto;
+import gov.cabinetofice.gapuserservice.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.ModelAndView;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
+    private final UserService userService;
+
     private static final String REGISTRATION_SUCCESS_VIEW = "something";
-    private static final String REGISTRATION_PAGE_VIEW = "something";
+    private static final String REGISTRATION_PAGE_VIEW = "register-user";
 
     @PostMapping("/register")
     public ModelAndView registerNewUser(@Valid @RequestBody CreateUserDto user, BindingResult result) {
@@ -21,7 +26,7 @@ public class UserController {
             return new ModelAndView(REGISTRATION_PAGE_VIEW);
         }
 
-        //createGrantApplicantService.createNewUser(user);
+        userService.createNewUser(user);
         return new ModelAndView(REGISTRATION_SUCCESS_VIEW);
     }
 }
