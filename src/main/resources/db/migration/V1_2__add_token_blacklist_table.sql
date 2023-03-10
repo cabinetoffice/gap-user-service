46de505735f00e34088b2a93277152958704377b
GavCookCO <99668051+GavCookCO@users.noreply.github.com>
2023-03-10 16:08:25 +0000

    Fixing my previously badly configured migration (#28)
    Co-authored-by: Gavin Cook <gavin.cook@and.digital>

--- src/main/resources/db/migration/V1_2__add_token_blacklist_table.sql
+++ src/main/resources/db/migration/V1_2__add_token_blacklist_table.sql
@@ -1,2 +1,6 @@
-create table token_blacklist (id integer not null, expiry_date timestamp(6), jwt varchar(4000), primary key (id));
-create index IDXs5liqeg06eex956w4sv6wuwau on token_blacklist (jwt);
+CREATE SEQUENCE IF NOT EXISTS token_blacklist_seq
+INCREMENT 1
+START 1;
+
+create table if not exists token_blacklist (id integer not null default nextval('token_blacklist_seq'::regclass), expiry_date timestamp(6), jwt varchar(4000), primary key (id));
+create index if not exists IDXs5liqeg06eex956w4sv6wuwau on token_blacklist (jwt);
