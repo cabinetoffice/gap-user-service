3214e1e64beb49714c294cabba3ee91bcb025c3d
john-tco <john.morley@cabinetoffice.gov.uk>
2023-06-27 18:06:45 +0100

    support roles table

--- src/main/resources/db/migration/V1_3__add_cognito_user_tables.sql
+++ src/main/resources/db/migration/V1_3__add_cognito_user_tables.sql
@@ -1,16 +1,28 @@
 CREATE TABLE IF NOT EXISTS gap_users (
   hashedEmail VARCHAR(512),
-  encryptedEmail VARCHAR(512)
+  encryptedEmail VARCHAR(1500),
   gap_user_id VARCHAR(64),
   sub VARCHAR(64),
-  roles VARCHAR(64),
   dept_id VARCHAR(64),
   PRIMARY KEY (sub)
 );
 
 CREATE TABLE IF NOT EXISTS departments (
   id VARCHAR(64),
+  name VARCHAR(256),
+  ggis_id VARCHAR(64),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+  id VARCHAR(64),
   name VARCHAR(64),
-  GGIS_id VARCHAR(64),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_roles (
+  id VARCHAR(64),
+  user_sub VARCHAR(64),
+  role_id VARCHAR(64),
   PRIMARY KEY (id)
 );
