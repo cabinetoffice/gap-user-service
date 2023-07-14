441b91df2403e34aa046376f0609f8a7e303dd9e
Dominic West <dominic.west@and.digital>
2023-07-14 16:57:41 +0100

    Adding get change department page endpoint

--- src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
+++ src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
@@ -1,6 +1,9 @@
 package gov.cabinetofice.gapuserservice.web;
 
+import gov.cabinetofice.gapuserservice.dto.ChangeDepartmentPageDto;
+import gov.cabinetofice.gapuserservice.dto.DepartmentDto;
 import gov.cabinetofice.gapuserservice.model.User;
+import gov.cabinetofice.gapuserservice.service.DepartmentService;
 import gov.cabinetofice.gapuserservice.service.user.OneLoginUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 
 @RequiredArgsConstructor
 @Controller
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     private final OneLoginUserService oneLoginUserService;
+    private final DepartmentService departmentService;
 
 
     @GetMapping("/user/{id}")
@@ -30,6 +36,16 @@ public class UserController {
                                                    @RequestParam("departmentId") Integer departmentId) {
         User user = oneLoginUserService.updateDepartment(id, departmentId);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping("/page/user/{userId}/change-department")
+    public ResponseEntity<ChangeDepartmentPageDto> getChangeDepartmentPage(@PathVariable("userId") Integer userId) {
+        final User user = oneLoginUserService.getUserById(userId);
+        final List<DepartmentDto> departments = departmentService.getAllDepartments();
+        return ResponseEntity.ok(ChangeDepartmentPageDto.builder()
+                .departments(departments)
+                .user(user)
+                .build());
     }
 
 }
