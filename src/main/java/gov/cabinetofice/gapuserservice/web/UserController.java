8b50af71759e897505e1217d21c38ea77081f86e
Dominic West <dominic.west@and.digital>
2023-07-19 16:52:37 +0100

    Throwing a 403 if accessing a SUPER_ADMIN only endpoint

--- src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
+++ src/main/java/gov/cabinetofice/gapuserservice/web/UserController.java
@@ -2,18 +2,17 @@ package gov.cabinetofice.gapuserservice.web;
 
 import gov.cabinetofice.gapuserservice.dto.ChangeDepartmentPageDto;
 import gov.cabinetofice.gapuserservice.dto.DepartmentDto;
+import gov.cabinetofice.gapuserservice.exceptions.ForbiddenException;
 import gov.cabinetofice.gapuserservice.model.User;
 import gov.cabinetofice.gapuserservice.service.DepartmentService;
+import gov.cabinetofice.gapuserservice.service.RoleService;
 import gov.cabinetofice.gapuserservice.service.user.OneLoginUserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,22 +24,35 @@ public class UserController {
 
     private final OneLoginUserService oneLoginUserService;
     private final DepartmentService departmentService;
+    private final RoleService roleService;
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable("id") Integer id) {
+    public ResponseEntity<User> getUserById(HttpServletRequest httpRequest, @PathVariable("id") Integer id) {
+        if (roleService.isSuperAdmin(httpRequest)) {
+            throw new ForbiddenException();
+        }
+
        return ResponseEntity.ok(oneLoginUserService.getUserById(id));
    }
 
     @PatchMapping("/user/{userId}/department")
-    public ResponseEntity<User> updateDepartment(@PathVariable("userId") Integer userId,
+    public ResponseEntity<User> updateDepartment(HttpServletRequest httpRequest, @PathVariable("userId") Integer userId,
                                                    @RequestParam(value = "departmentId", required = false) Integer departmentId) {
+        if (roleService.isSuperAdmin(httpRequest)) {
+            throw new ForbiddenException();
+        }
+
         if(departmentId == null) return ResponseEntity.ok().build();
         User user = oneLoginUserService.updateDepartment(userId, departmentId);
         return ResponseEntity.ok(user);
     }
 
     @GetMapping("/page/user/{userId}/change-department")
-    public ResponseEntity<ChangeDepartmentPageDto> getChangeDepartmentPage(@PathVariable("userId") Integer userId) {
+    public ResponseEntity<ChangeDepartmentPageDto> getChangeDepartmentPage(HttpServletRequest httpRequest, @PathVariable("userId") Integer userId) {
+        if (roleService.isSuperAdmin(httpRequest)) {
+            throw new ForbiddenException();
+        }
+
         final User user = oneLoginUserService.getUserById(userId);
         final List<DepartmentDto> departments = departmentService.getAllDepartments();
         return ResponseEntity.ok(ChangeDepartmentPageDto.builder()
@@ -50,8 +62,12 @@ public class UserController {
     }
 
     @PatchMapping("/user/{id}/role")
-    public ResponseEntity<String> updateRoles(@RequestBody() List<Integer> roleIds,
-                                @PathVariable("id") Integer id) {
+    public ResponseEntity<String> updateRoles(HttpServletRequest httpRequest, @RequestBody() List<Integer> roleIds,
+                                              @PathVariable("id") Integer id) {
+        if (roleService.isSuperAdmin(httpRequest)) {
+            throw new ForbiddenException();
+        }
+
         oneLoginUserService.updateRoles(id, roleIds);
         return ResponseEntity.ok("success");
     }
