acc9993698f2b2934255889c76f2c202ea111b62
john-tco <john.morley@cabinetoffice.gov.uk>
2023-07-24 15:08:45 +0100

    add ref data

---
+++ src/main/resources/db/migration/V1_7__populate_department_table.sql
@@ -0,0 +1,22 @@
+ALTER TABLE departments
+ADD type varchar(255) NULL;
+
+INSERT INTO departments (name, type, ggis_id) VALUES
+  ('Cabinet Office', 'Central Department', '0012400000MNijpAAD'),
+  ('Department for Business, Energy and Industrial Strategy', 'Central Department', '0012400001IJifPAAT'),
+  ('Department for Digital, Culture, Media & Sport', 'Central Department', '0012400000MNijwAAD'),
+  ('Department for Education', 'Central Department', '0012400000MNijyAAD'),
+  ('Department for Environment, Food and Rural Affairs', 'Central Department', '0012400000MNik6AAD'),
+  ('Department for International Trade', 'Central Department', '001240000149naSAAQ'),
+  ('Department for Levelling Up, Housing and Communities', 'Central Department', '0012400000MNijsAAD'),
+  ('Department for Transport', 'Central Department', '0012400000MNljCAAT'),
+  ('Department for Work and Pensions', 'Central Department', '0012400000MNik4AAD'),
+  ('Department for Health and Social Care', 'Central Department', '0012400000MNik3AAD'),
+  ('Foreign, Commonwealth & Development Office', 'Central Department', '0012400000MNik8AAD'),
+  ('HM Revenue & Customs', 'Central Department', '0012400000MNlt7AAD'),
+  ('HM Treasury', 'Central Department', '0012400000MNikEAAT'),
+  ('Home Office', 'Central Department', '0012400000MNltvAAD'),
+  ('Ministry of Defence', 'Central Department', '0012400000MNikMAAT'),
+  ('Ministry of Justice', 'Central Department', '0012400000MNikNAAT');
+
+
