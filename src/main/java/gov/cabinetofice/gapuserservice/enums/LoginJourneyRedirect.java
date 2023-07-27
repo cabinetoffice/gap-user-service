714554fbd6a123b8e3e53e9ad90812325dadae95
Dominic West <dominic.west@and.digital>
2023-07-27 17:53:53 +0100

    Refactoring LoginJourneyRedirect getRedirectUrl function

--- src/main/java/gov/cabinetofice/gapuserservice/enums/LoginJourneyRedirect.java
+++ src/main/java/gov/cabinetofice/gapuserservice/enums/LoginJourneyRedirect.java
@@ -3,17 +3,30 @@ package gov.cabinetofice.gapuserservice.enums;
 import static gov.cabinetofice.gapuserservice.web.LoginControllerV2.PRIVACY_POLICY_PAGE_VIEW;
 
 public enum LoginJourneyRedirect {
-    PRIVACY_POLICY_PAGE,
-    SUPER_ADMIN_DASHBOARD,
-    ADMIN_DASHBOARD,
-    APPLICANT_APP;
+    PRIVACY_POLICY_PAGE{
+        @Override
+        public String getRedirectUrl(String adminBaseUrl, String redirectUrlCookie) {
+            return PRIVACY_POLICY_PAGE_VIEW;
+        }
+    },
+    SUPER_ADMIN_DASHBOARD {
+        @Override
+        public String getRedirectUrl(String adminBaseUrl, String redirectUrlCookie) {
+            return adminBaseUrl + "?redirectUrl=/super-admin-dashboard";
+        }
+    },
+    ADMIN_DASHBOARD {
+        @Override
+        public String getRedirectUrl(String adminBaseUrl, String redirectUrlCookie) {
+            return adminBaseUrl + "?redirectUrl=/dashboard";
+        }
+    },
+    APPLICANT_APP {
+        @Override
+        public String getRedirectUrl(String adminBaseUrl, String redirectUrlCookie) {
+            return redirectUrlCookie;
+        }
+    };
 
-    public String getRedirectUrl(final String adminBaseUrl, final String redirectUrlCookie) {
-        return switch (this) {
-            case SUPER_ADMIN_DASHBOARD -> adminBaseUrl + "?redirectUrl=/super-admin-dashboard";
-            case ADMIN_DASHBOARD -> adminBaseUrl + "?redirectUrl=/dashboard";
-            case PRIVACY_POLICY_PAGE -> PRIVACY_POLICY_PAGE_VIEW;
-            case APPLICANT_APP -> redirectUrlCookie;
-        };
-    }
+    public abstract String getRedirectUrl(String adminBaseUrl, String redirectUrlCookie);
 }
