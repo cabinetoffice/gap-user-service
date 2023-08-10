30f2580c16374134f0903c42bfe73d72454e1fe0
Gavin Cook <gavin.cook1@cabinetoffice.gov.uk>
2023-08-10 16:13:11 +0100

    Removing todo

--- src/main/java/gov/cabinetofice/gapuserservice/model/Role.java
+++ src/main/java/gov/cabinetofice/gapuserservice/model/Role.java
@@ -15,7 +15,7 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "roles") //TODO database table names are usually singular
+@Table(name = "roles")
 public class Role {
 
     @Id
