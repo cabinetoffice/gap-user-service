bac22daaf78464bc1487a617f32e602ee59b1f39
conor <conor.fayle@and.digital>
2023-08-22 16:36:56 +0100

    GAP-2092|change 418 code to 409

--- src/main/java/gov/cabinetofice/gapuserservice/exceptions/NonceExpiredException.java
+++ src/main/java/gov/cabinetofice/gapuserservice/exceptions/NonceExpiredException.java
@@ -3,7 +3,7 @@ package gov.cabinetofice.gapuserservice.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+@ResponseStatus(HttpStatus.CONFLICT)
 public class NonceExpiredException extends RuntimeException {
 
     public NonceExpiredException(String message) {
