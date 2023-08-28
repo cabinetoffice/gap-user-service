2fb6af045ef0e339800bf32dd63ff2fd1f713e8b
dylanwrightCO <135221918+dylanwrightCO@users.noreply.github.com>
2023-08-28 19:58:02 +0100

    Gap 2132 fix user request (#102)
    * fix(user-request): adds user request to token filter as called via lambda
    
    * reverting commented out code
    
    * removing test
    
    ---------
    
    Co-authored-by: Dylan Wright <dylan.wright@and.digital>

---
+++ src/test/java/gov/cabinetofice/gapuserservice/service/SecretAuthServiceTest.java
@@ -0,0 +1,35 @@
+package gov.cabinetofice.gapuserservice.service;
+
+import gov.cabinetofice.gapuserservice.exceptions.UnauthorizedException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+public class SecretAuthServiceTest {
+    @InjectMocks
+    private SecretAuthService secretAuthService;
+
+    @Test
+    void shouldNotThrowUnauthorisedExceptionWhenSecretIsValid() {
+
+        ReflectionTestUtils.setField(secretAuthService, "lambdaSecret", "validSecret");
+        assertDoesNotThrow(() -> secretAuthService.authenticateSecret("validSecret"));
+
+    }
+
+    @Test
+    void shouldThrowUnauthorisedExceptionWhenSecretIsInValid() {
+
+        ReflectionTestUtils.setField(secretAuthService, "lambdaSecret", "validSecret");
+        assertThrows(UnauthorizedException.class, () ->
+                secretAuthService.authenticateSecret("InvalidSecret"));
+
+    }
+
+}
