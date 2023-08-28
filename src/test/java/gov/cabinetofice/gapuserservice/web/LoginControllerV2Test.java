636be2a7a239eecbc0e32731d347b8f15fe4a9ab
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-08-28 09:59:48 +0100

    fix test

--- src/test/java/gov/cabinetofice/gapuserservice/web/LoginControllerV2Test.java
+++ src/test/java/gov/cabinetofice/gapuserservice/web/LoginControllerV2Test.java
@@ -122,9 +122,9 @@ class LoginControllerV2Test {
 
             assertThat(stateCookie.getName()).isEqualTo(loginController.getSTATE_COOKIE());
             assertThat(stateCookie.getValue()).isEqualTo("eyJyZWRpcmVjdFVybCI6Imh0dHBzOi8vd3d3LmZpbmQtZ292ZXJubWVudC1ncmFudHMuc2VydmljZS5nb3YudWsvIiwic3RhdGUiOiJzdGF0ZSJ9");
-            assertThat(stateCookie.isHttpOnly()).isEqualTo("true");
-            assertThat(stateCookie.getSecure()).isEqualTo("true");
-            assertThat(stateCookie.getMaxAge()).isEqualTo("3600");
+            assertThat(stateCookie.isHttpOnly()).isEqualTo(true);
+            assertThat(stateCookie.getSecure()).isEqualTo(true);
+            assertThat(stateCookie.getMaxAge()).isEqualTo(3600);
 
             assertThat(methodResponse.getUrl()).isEqualTo("loginUrl");
         }
