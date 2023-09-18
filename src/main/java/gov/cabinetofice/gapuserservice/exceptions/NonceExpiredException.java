c8fd76a0c64ad5105ead7d363ce5f81dd50a6813
conor <conor.fayle@and.digital>
2023-09-18 11:58:32 +0100

    GAP-2092|fix session expiry page - throw 403 instead of 409, return specific error page instead of using spring magic

--- src/main/java/gov/cabinetofice/gapuserservice/exceptions/NonceExpiredException.java
+++ src/main/java/gov/cabinetofice/gapuserservice/exceptions/NonceExpiredException.java
@@ -3,7 +3,7 @@ package gov.cabinetofice.gapuserservice.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class NonceExpiredException extends RuntimeException {
 
     public NonceExpiredException(String message) {
