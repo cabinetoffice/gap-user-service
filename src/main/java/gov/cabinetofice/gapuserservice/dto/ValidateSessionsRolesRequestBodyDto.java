0b38c32d45ff211e1ff6e51e9dabbc6d4b2e1806
john-tco <john.morley@cabinetoffice.gov.uk>
2023-09-22 11:47:06 +0100

    rm

--- src/main/java/gov/cabinetofice/gapuserservice/dto/ValidateSessionsRolesRequestBodyDto.java
+++ src/main/java/gov/cabinetofice/gapuserservice/dto/ValidateSessionsRolesRequestBodyDto.java
@@ -1,11 +1,7 @@
 package gov.cabinetofice.gapuserservice.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Data
 public class ValidateSessionsRolesRequestBodyDto {
     private String emailAddress;
