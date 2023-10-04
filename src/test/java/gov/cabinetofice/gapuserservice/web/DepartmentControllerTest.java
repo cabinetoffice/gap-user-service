8634b799a500f9d6434aac5435a5d77f0097df51
paul-lawlor-tco <paul.lawlor1@cabinetoffice.gov.uk>
2023-10-04 11:33:58 +0100

    Fixed one issue in DepartmentControllerTest.java. Removed unneccesary public modifier for test class

--- src/test/java/gov/cabinetofice/gapuserservice/web/DepartmentControllerTest.java
+++ src/test/java/gov/cabinetofice/gapuserservice/web/DepartmentControllerTest.java
@@ -22,7 +22,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class DepartmentControllerTest {
+class DepartmentControllerTest {
 
     @InjectMocks
     private DepartmentController departmentController;
