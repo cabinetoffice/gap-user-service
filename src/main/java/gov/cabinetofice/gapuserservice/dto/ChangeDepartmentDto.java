404a69d2702cef48f4d28667936c18030863c7ec
paul-lawlor-tco <145372589+paul-lawlor-tco@users.noreply.github.com>
2023-12-22 14:58:43 +0000

    GAP-2072 Adding annotation to prevent lack of default constructor causing error on sandbox. (#170)

--- src/main/java/gov/cabinetofice/gapuserservice/dto/ChangeDepartmentDto.java
+++ src/main/java/gov/cabinetofice/gapuserservice/dto/ChangeDepartmentDto.java
@@ -3,9 +3,11 @@ package gov.cabinetofice.gapuserservice.dto;
 import gov.cabinetofice.gapuserservice.validation.annotations.FieldNotNull;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
 public class ChangeDepartmentDto {
     @FieldNotNull(message = "Select a department")
     private Integer departmentId;
